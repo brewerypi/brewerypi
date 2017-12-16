3719ab7bf0924c9e65e8239064297d9a5d6953a1
Brian Faivre <brian.faivre@gmail.com>
2017-12-16 08:47:57 -0800

    Fixes #17.

--- app/lookupValues/forms.py
+++ app/lookupValues/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import HiddenField, IntegerField, StringField, SubmitField
-from wtforms.validators import Length, Required
+from wtforms.validators import Length, NumberRange, Required
 
 class LookupValueForm(FlaskForm):
 	lookupId = HiddenField()
 	name = StringField("Name", validators = [Required(), Length(1, 45)])
-	value = IntegerField("Value", validators = [Required()])
+	value = IntegerField("Value", validators = [NumberRange(min = 0)])
 	submit = SubmitField("Save")
