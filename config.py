b8cf312519715982ea9a4f1f8fc5605b7ad18eef
Brian Faivre <brian.faivre@gmail.com>
2017-12-24 13:06:11 -0800

    Tag export and import implemented with add and edit functionality.

--- config.py
+++ config.py
@@ -2,7 +2,10 @@ import os
 from dotenv import load_dotenv
 
 class Config:
+	EXPORT_FOLDER = "exports"
+	EXPORT_TAGS_FILENAME = "tags.csv"
+	IMPORT_FOLDER = "imports"
+	IMPORT_TAGS_FILENAME = "tags.csv"
 	SECRET_KEY = os.environ.get("SECRET_KEY") or "p9Bv<3Eid9%$i01"
-	SQLALCHEMY_DATABASE_URI = os.environ.get("SECRET_KEY") or "mysql://pi:brewery@localhost/BreweryPiDemo1"
+	SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI") or "mysql://pi:brewery@localhost/BreweryPiDemo1"
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
-	TAG_IMPORT_FILE_PATH = "imports/tags.csv"
