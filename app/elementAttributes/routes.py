d6f5278cb704ec324ad4d5539098792852c2f1c4
Brian Faivre <brian.faivre@gmail.com>
2018-01-27 16:19:47 -0800

    Redirect element attribute value changes back to the element dashboard.

--- app/elementAttributes/routes.py
+++ app/elementAttributes/routes.py
@@ -253,7 +253,7 @@ def addElementAttributeValue(elementId, tagId):
 		db.session.add(tagValue)
 		db.session.commit()
 		flash("You have successfully added a new element attribute value.")
-		return redirect(url_for("elementAttributes.listElementAttributeValues", elementId = elementId))
+		return redirect(url_for("elements.dashboard", elementId = elementId))
 
 	# Present a form to add a new element attribute value.
 	form.tagId.data = tag.TagId
@@ -267,7 +267,7 @@ def deleteElementAttributeValue(elementId, tagValueId):
 	db.session.delete(tagValue)
 	db.session.commit()
 	flash("You have successfully deleted the element attribute value.")
-	return redirect(url_for("elementAttributes.listElementAttributeValues", elementId = elementId))
+	return redirect(url_for("elements.dashboard", elementId = elementId))
 
 @elementAttributes.route("/elementAttributes/editValue/<int:elementId>/<int:tagValueId>", methods = ["GET", "POST"])
 # @login_required
@@ -299,7 +299,7 @@ def editElementAttributeValue(elementId, tagValueId):
 
 		db.session.commit()
 		flash("You have successfully edited the element attribute value.")
-		return redirect(url_for("elementAttributes.listElementAttributeValues", elementId = elementId))
+		return redirect(url_for("elements.dashboard", elementId = elementId))
 
 	# Present a form to edit an existing element attribute value.
 	form.tagId.data = tagValue.TagId
