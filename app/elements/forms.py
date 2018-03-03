0be1f1de47812cdbfe1e288d1bfb5ef4b24a5547
Brian Faivre <brian.faivre@gmail.com>
2018-03-03 11:44:28 -0800

    Fixes #72. Resolves #71.

--- app/elements/forms.py
+++ app/elements/forms.py
@@ -1,13 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import HiddenField, StringField, SubmitField
 from wtforms.ext.sqlalchemy.fields import QuerySelectField
-from wtforms.validators import Length, Required
+from wtforms.validators import Length, Optional, Required
 from .. helpers import elementTemplateFullyAbbreviatedPath
 from .. models import Element, ElementTemplate, Enterprise, Site
 
 class ElementForm(FlaskForm):
 	elementTemplate = QuerySelectField("Element Template", query_factory = lambda: ElementTemplate.query.join(Site, Enterprise). \
-		order_by(Enterprise.Abbreviation, Site.Abbreviation, ElementTemplate.Name), get_label = elementTemplateFullyAbbreviatedPath)
+		order_by(Enterprise.Abbreviation, Site.Abbreviation, ElementTemplate.Name), get_label = elementTemplateFullyAbbreviatedPath, validators = [Optional()])
 	name = StringField("Name", validators = [Required(), Length(1, 45)])
 	description = StringField("Description", validators = [Length(0, 255)])
+	elementIdToCopy = HiddenField()
 	submit = SubmitField("Save")
