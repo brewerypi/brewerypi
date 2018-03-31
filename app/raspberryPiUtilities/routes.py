696cea701d2d296e773c634d21a280903d204521
Brian Faivre <brian.faivre@gmail.com>
2018-03-30 20:30:17 -0700

    Draft of shutdown command.

--- app/raspberryPiUtilities/routes.py
+++ app/raspberryPiUtilities/routes.py
@@ -4,10 +4,21 @@ from flask_login import login_required
 from . import raspberryPiUtilities
 from .. decorators import adminRequired
 
-@raspberryPiUtilities.route("/reboot", methods = ["GET", "POST"])
+@raspberryPiUtilities.route("/reboot", methods = ["GET"])
 @login_required
 @adminRequired
 def reboot():
 	flash("Rebooting... Please wait. This will take approximately one minute.")
-	subprocess.call(["sudo", "reboot"])
+	command = "sleep 5 && sudo shutdown -r now"
+	subprocess.Popen(command, shell = True)
+	return redirect(url_for("home.homepage"))
+
+@raspberryPiUtilities.route("/shutdown", methods = ["GET"])
+@login_required
+@adminRequired
+def shutdown():
+	flash("Shutting down... Please wait.")
+	flash("When the LEDs on the Raspberry Pi stop blinking, it should be safe to unplug your Raspberry Pi.")
+	command = "sleep 5 && sudo shutdown -h now"
+	subprocess.Popen(command, shell = True)
 	return redirect(url_for("home.homepage"))
