626db222b8e8127686641c693cd5ef9a4ed22f57
Brian Faivre <brian.faivre@gmail.com>
2018-07-13 12:40:09 -0700

    Moved element attributes functionality with elements.

--- app/elementAttributeTemplates/forms.py
+++ app/elementAttributeTemplates/forms.py
@@ -1,13 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import HiddenField, StringField, SubmitField
-# from wtforms.ext.sqlalchemy.fields import QuerySelectField
 from wtforms.validators import Length, Required
-# from .. helpers import elementTemplateFullyAbbreviatedPath
-# from .. models import ElementTemplate, Enterprise, Site
 
 class ElementAttributeTemplateForm(FlaskForm):
-	# elementTemplate = QuerySelectField("Element Template", query_factory = lambda: ElementTemplate.query.join(Site, Enterprise). \
-	# 	order_by(Enterprise.Abbreviation, Site.Abbreviation, ElementTemplate.Name), get_label = elementTemplateFullyAbbreviatedPath)
 	elementTemplateId = HiddenField()
 	name = StringField("Name", validators = [Required(), Length(1, 45)])
 	description = StringField("Description", validators = [Length(0, 255)])
