6a8c127e3e349690ef3e0cf6066230a463608ac2
Brian Faivre <brian.faivre@gmail.com>
2018-07-16 07:47:25 -0700

    Fixed bug and cleaned up formatting.

--- app/elementAttributes/routes.py
+++ app/elementAttributes/routes.py
@@ -110,7 +110,8 @@ def exportElementAttributes():
 		order_by(Enterprise.Abbreviation, Site.Abbreviation, ElementTemplate.Name, Element.Name, ElementAttributeTemplate.Name)
 	with open(os.path.join(current_app.config["EXPORT_FOLDER"], current_app.config["EXPORT_ELEMENT_ATTRIBUTES_FILENAME"]), "w", encoding = "latin-1") \
 		as elementsFile:
-		fieldnames = ["Selected", "Element Attribute Id", "Enterprise", "Site", "Element Template", "Element Name", "Element Attribute Template", "Area", "Tag Name"]
+		fieldnames = ["Selected", "Element Attribute Id", "Enterprise", "Site", "Element Template", "Element Name", "Element Attribute Template", "Area",
+			"Tag Name"]
 		elementAttributesWriter = csv.DictWriter(elementsFile, fieldnames = fieldnames, lineterminator = "\n")
 		elementAttributesWriter.writeheader()
 		
@@ -147,99 +148,104 @@ def importElementAttributes():
 			if "Selected" not in elementAttributesReader.fieldnames or "Element Attribute Id" not in elementAttributesReader.fieldnames or \
 				"Enterprise" not in elementAttributesReader.fieldnames or "Site" not in elementAttributesReader.fieldnames or \
 				"Element Template" not in elementAttributesReader.fieldnames or "Element Name" not in elementAttributesReader.fieldnames or \
-				"Attribute Template" not in elementAttributesReader.fieldnames or "Area" not in elementAttributesReader.fieldnames or \
+				"Element Attribute Template" not in elementAttributesReader.fieldnames or "Area" not in elementAttributesReader.fieldnames or \
 				"Tag Name" not in elementAttributesReader.fieldnames:
 				errors.append("Header malformed. Aborting upload.")
-				return render_template("elementAttributes/importElementAttributes.html", errors = errors, form = form)
+			else:
+				# Iterate through each row.
+				for n, row in enumerate(elementAttributesReader, start = 1):
+					rowNumber = n + 1
+					selected = row["Selected"].strip()
+					elementAttributeId = row["Element Attribute Id"].strip()
+					enterpriseAbbreviation = row["Enterprise"].strip()
+					siteAbbreviation = row["Site"].strip()
+					elementTemplateName = row["Element Template"].strip()
+					elementName = row["Element Name"].strip()
+					elementAttributeTemplateName = row["Element Attribute Template"].strip()
+					areaAbbreviation = row["Area"].strip()
+					tagName = row["Tag Name"].strip()
 
-			# Iterate through each row.
-			for n, row in enumerate(elementAttributesReader, start = 1):
-				rowNumber = n + 1
-				selected = row["Selected"].strip()
-				elementAttributeId = row["Element Attribute Id"].strip()
-				enterpriseAbbreviation = row["Enterprise"].strip()
-				siteAbbreviation = row["Site"].strip()
-				elementTemplateName = row["Element Template"].strip()
-				elementName = row["Element Name"].strip()
-				elementAttributeTemplateName = row["Element Attribute Template"].strip()
-				areaAbbreviation = row["Area"].strip()
-				tagName = row["Tag Name"].strip()
-
-				# Skip rows that are now selected.
-				if "" == selected:
-					warnings.append("Row " + str(rowNumber) + " not selected. Skipping row " + str(rowNumber) + ".")
-					continue
-
-				enterprise = Enterprise.query.filter(Enterprise.Abbreviation == enterpriseAbbreviation).first()
-				if enterprise is None:
-					errors.append("Enterprise \"" + enterpriseAbbreviation + "\" does not exist. Skipping row " + str(rowNumber) + ".")
-					continue
-
-				site = Site.query.filter(Site.Abbreviation == siteAbbreviation, Site.EnterpriseId == enterprise.EnterpriseId).first()
-				if site is None:
-					errors.append("Site \"" + siteAbbreviation + "\" does not exist. Skipping row " + str(rowNumber) + ".")
-					continue
-
-				elementTemplate = ElementTemplate.query.filter(ElementTemplate.Name == elementTemplateName, ElementTemplate.SiteId == site.SiteId).first()
-				if elementTemplate is None:
-					errors.append("Element Template \"" + elementTemplateName + "\" does not exist in site \"" + site.Name + "\". Skipping row " + \
-						str(rowNumber) + ".")
-					continue
-
-				elementAttributeTemplate = ElementAttributeTemplate.query.filter(ElementAttributeTemplate.ElementTemplateId == elementTemplate.ElementTemplateId, ElementAttributeTemplate.Name == elementAttributeTemplateName).first()
-				if elementAttributeTemplate is None:
-					errors.append("Element Attribute Template \"" + elementAttributeTemplateName + "\" does not exist in site \"" + site.Name + "\". Skipping row " + str(rowNumber) + ".")
-					continue
-
-				area = Area.query.filter(Area.Abbreviation == areaAbbreviation, Area.SiteId == site.SiteId).first()
-				if area is None:
-					errors.append("Area \"" + areaAbbreviation + "\" does not exist. Skipping row " + str(rowNumber) + ".")
-					continue
-
-				tag = Tag.query.filter(Tag.AreaId == area.AreaId, Tag.Name == tagName).first()
-				if tag is None:
-					errors.append("Tag \"" + tagName + "\" does not exist in area \"" + area.Name + "\". Skipping row " + str(rowNumber) + ".")
-					continue
-
-				element = Element.query.filter(Element.ElementTemplateId == elementTemplate.ElementTemplateId, Element.Name == elementName).first()
-				if element is None:
-					# Add element.
-					element = Element(ElementTemplate = elementTemplate, Name = elementName)
-					db.session.add(element)
-					db.session.commit()
-					successes.append("Element \"" + elementName + "\" added.")
-
-				if "" == elementAttributeId:
-					# Add element attribute.
-					# Check for existing element attribute.
-					elementAttribute = ElementAttribute.query.filter(ElementAttribute.ElementAttributeTemplateId == elementAttributeTemplate.ElementAttributeTemplateId, ElementAttribute.ElementId == element.ElementId).first()
-					if elementAttribute is not None:
-						warnings.append("Element Attribute Template \"" + elementAttributeTemplateName + "\" already exists for Element \"" + elementName + \
-							"\". Skipping row " + str(rowNumber) + ".")
+					# Skip rows that are now selected.
+					if "" == selected:
+						warnings.append("Row {} not selected. Skipping row {}.".format(str(rowNumber), str(rowNumber)))
 						continue
 
-					# Add element attribute.
-					elementAttribute = ElementAttribute(ElementAttributeTemplate = elementAttributeTemplate, Element = element, Tag = tag)
-					db.session.add(elementAttribute)
-					db.session.commit()
-					successes.append("Element Attribute Template \"" + elementAttributeTemplateName + "\" added to Element \"" + elementName + "\".")
-				else:
-					# Edit element attribute.
-					elementAttribute = ElementAttribute.query.get_or_404(elementAttributeId)
+					enterprise = Enterprise.query.filter(Enterprise.Abbreviation == enterpriseAbbreviation).first()
+					if enterprise is None:
+						errors.append("Enterprise \"{}\" does not exist. Skipping row {}.".format(enterpriseAbbreviation, str(rowNumber)))
+						continue
 
-					# The element attribute template and/or element have changed. Check for existing element attribute.
-					if elementAttribute.ElementAttributeTemplateId != elementAttributeTemplate.ElementAttributeTemplateId or elementAttribute.ElementId != element.ElementId:
-						elementAttributeCheck = ElementAttribute.query.join(ElementAttributeTemplate, Element).filter(ElementAttribute.ElementAttributeTemplateId == elementAttributeTemplate.ElementAttributeTemplateId, \
+					site = Site.query.filter(Site.Abbreviation == siteAbbreviation, Site.EnterpriseId == enterprise.EnterpriseId).first()
+					if site is None:
+						errors.append("Site \"{}\" does not exist. Skipping row {}.".format(siteAbbreviation, str(rowNumber)))
+						continue
+
+					elementTemplate = ElementTemplate.query.filter(ElementTemplate.Name == elementTemplateName, ElementTemplate.SiteId == site.SiteId).first()
+					if elementTemplate is None:
+						errors.append("Element Template \"{}\" does not exist in site \"{}\". Skipping row {}.".format(elementTemplateName, site.Name,
+							str(rowNumber)))
+						continue
+
+					elementAttributeTemplate = ElementAttributeTemplate.query. \
+						filter(ElementAttributeTemplate.ElementTemplateId == elementTemplate.ElementTemplateId,
+						ElementAttributeTemplate.Name == elementAttributeTemplateName).first()
+					if elementAttributeTemplate is None:
+						errors.append("Element Attribute Template \"{}\" does not exist in site \"{}\". Skipping row {}.".format(elementAttributeTemplateName,
+							site.Name, str(rowNumber)))
+						continue
+
+					area = Area.query.filter(Area.Abbreviation == areaAbbreviation, Area.SiteId == site.SiteId).first()
+					if area is None:
+						errors.append("Area \"{}\" does not exist. Skipping row {}.".format(areaAbbreviation, str(rowNumber)))
+						continue
+
+					tag = Tag.query.filter(Tag.AreaId == area.AreaId, Tag.Name == tagName).first()
+					if tag is None:
+						errors.append("Tag \"{}\" does not exist in area \"{}\". Skipping row {}.".format(tagName, area.Name, str(rowNumber)))
+						continue
+
+					element = Element.query.filter(Element.ElementTemplateId == elementTemplate.ElementTemplateId, Element.Name == elementName).first()
+					if element is None:
+						# Add element.
+						element = Element(ElementTemplate = elementTemplate, Name = elementName)
+						db.session.add(element)
+						db.session.commit()
+						successes.append("Element \"{}\" added.".format(elementName))
+
+					if "" == elementAttributeId:
+						# Check for existing element attribute.
+						elementAttribute = ElementAttribute.query. \
+							filter(ElementAttribute.ElementAttributeTemplateId == elementAttributeTemplate.ElementAttributeTemplateId,
 							ElementAttribute.ElementId == element.ElementId).first()
-						if elementAttributeCheck is not None:
-							warnings.append("Element Attribute Template  \"" + elementAttributeName + "\" already exists for Element \"" + elementName + \
-								"\". Skipping row " + str(rowNumber) + ".")
+						if elementAttribute is not None:
+							warnings.append("Element Attribute Template \"{}\" already exists for Element \"{}\". Skipping row {}.". \
+								format(elementAttributeTemplateName, elementName, str(rowNumber)))
 							continue
-							
-					elementAttribute.ElementAttributeTemplate = elementAttributeTemplate
-					elementAttribute.Element = element
-					elementAttribute.Tag = tag
-					db.session.commit()
-					successes.append("Element Attribute Template \"" + elementAttributeTemplateName + "\" updated for Element \"" + elementName + "\".")
+
+						# Add element attribute.
+						elementAttribute = ElementAttribute(ElementAttributeTemplate = elementAttributeTemplate, Element = element, Tag = tag)
+						db.session.add(elementAttribute)
+						db.session.commit()
+						successes.append("Element Attribute Template \"{}\" added to Element \"{}\".".format(elementAttributeTemplateName, elementName))
+					else:
+						# Edit element attribute.
+						elementAttribute = ElementAttribute.query.get_or_404(elementAttributeId)
+
+						# The element attribute template and/or element have changed. Check for existing element attribute.
+						if elementAttribute.ElementAttributeTemplateId != elementAttributeTemplate.ElementAttributeTemplateId or \
+							elementAttribute.ElementId != element.ElementId:
+							elementAttributeCheck = ElementAttribute.query.join(ElementAttributeTemplate, Element). \
+								filter(ElementAttribute.ElementAttributeTemplateId == elementAttributeTemplate.ElementAttributeTemplateId, \
+								ElementAttribute.ElementId == element.ElementId).first()
+							if elementAttributeCheck is not None:
+								warnings.append("Element Attribute Template  \"{}\" already exists for Element \"{}\". Skipping row {}.". \
+									format(elementAttributeName, elementName, str(rowNumber)))
+								continue
+								
+						elementAttribute.ElementAttributeTemplate = elementAttributeTemplate
+						elementAttribute.Element = element
+						elementAttribute.Tag = tag
+						db.session.commit()
+						successes.append("Element Attribute Template \"{}\" updated for Element \"{}\".".format(elementAttributeTemplateName, elementName))
 
 	return render_template("import.html", errors = errors, form = form, importing = "Element Attributes", successes = successes, warnings = warnings)
