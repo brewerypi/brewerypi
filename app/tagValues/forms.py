8ddfb2144efe81e3c5c3995ced5ed51431bd5d4c
Brian Faivre <brian.faivre@gmail.com>
2018-09-11 18:55:11 -0700

    Moved value to be the 1st field and timestamp the 2nd.

--- app/tagValues/forms.py
+++ app/tagValues/forms.py
@@ -5,8 +5,8 @@ from wtforms.validators import Required
 
 class TagValueForm(FlaskForm):
 	tagId = HiddenField()
-	timestamp = DateTimeField("Timestamp", default = datetime.now, validators = [Required()])
 	value = FloatField("Value")
+	timestamp = DateTimeField("Timestamp", default = datetime.now, validators = [Required()])
 	lookupValue = SelectField("Lookup", coerce = float)
 	requestReferrer = HiddenField()
 	submit = SubmitField("Save")
