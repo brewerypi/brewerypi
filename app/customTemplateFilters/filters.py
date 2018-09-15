a1daf6da1985d56d93b9aeec5ddb0eb1eddb21d4
Brian Faivre <brian.faivre@gmail.com>
2018-09-15 11:19:49 -0700

    Draf to make Grafana URI compatible w/Windows and Linux.

--- app/customTemplateFilters/filters.py
+++ app/customTemplateFilters/filters.py
@@ -6,11 +6,11 @@ from .. models import Element, ElementTemplate, EventFrame, EventFrameTemplate, 
 @customTemplateFilters.app_template_filter()
 def grafanaUrl(uid, parameters = None):
     if uid == "":
-        return "http://{}:{}".format(current_app.config["GRAFANA_HOSTNAME"], current_app.config["GRAFANA_PORT"])
+        return current_app.config["GRAFANA_BASE_URI"]
     elif uid == "ActiveEventFrameSummary":
         if "elementTemplateId" in parameters:
             elementTemplate = ElementTemplate.query.get_or_404(parameters["elementTemplateId"])
-            return "http://{}:{}".format(current_app.config["GRAFANA_HOSTNAME"], current_app.config["GRAFANA_PORT"]) + \
+            return current_app.config["GRAFANA_BASE_URI"] + \
                 "/d/ActiveEventFrameSummary/active-event-frame-summary?orgId=1" + \
                 "&var-enterprise={}".format(elementTemplate.Site.Enterprise.EnterpriseId) + \
                 "&var-site={}".format(elementTemplate.Site.SiteId) + \
@@ -19,7 +19,7 @@ def grafanaUrl(uid, parameters = None):
                 "&var-eventFrameAttributeTemplates=All"
         elif "eventFrameTemplateId" in parameters:
             eventFrameTemplate = EventFrameTemplate.query.get_or_404(parameters["eventFrameTemplateId"])
-            return "http://{}:{}".format(current_app.config["GRAFANA_HOSTNAME"], current_app.config["GRAFANA_PORT"]) + \
+            return current_app.config["GRAFANA_BASE_URI"] + \
                 "/d/ActiveEventFrameSummary/active-event-frame-summary?orgId=1" + \
                 "&var-enterprise={}".format(eventFrameTemplate.ElementTemplate.Site.Enterprise.EnterpriseId) + \
                 "&var-site={}".format(eventFrameTemplate.ElementTemplate.Site.SiteId) + \
@@ -28,7 +28,7 @@ def grafanaUrl(uid, parameters = None):
                 "&var-eventFrameAttributeTemplates=All"
         elif "siteId" in parameters:
             site = Site.query.get_or_404(parameters["siteId"])
-            return "http://{}:{}".format(current_app.config["GRAFANA_HOSTNAME"], current_app.config["GRAFANA_PORT"]) + \
+            return current_app.config["GRAFANA_BASE_URI"] + \
                 "/d/ActiveEventFrameSummary/active-event-frame-summary?orgId=1" + \
                 "&var-enterprise={}".format(site.Enterprise.EnterpriseId) + \
                 "&var-site={}".format(site.SiteId) + \
@@ -38,7 +38,7 @@ def grafanaUrl(uid, parameters = None):
     elif uid == "ElementSummary":
         if "siteId" in parameters:
             site = Site.query.get_or_404(parameters["siteId"])
-            return "http://{}:{}".format(current_app.config["GRAFANA_HOSTNAME"], current_app.config["GRAFANA_PORT"]) + \
+            return current_app.config["GRAFANA_BASE_URI"] + \
                 "/d/ElementSummary/element-summary?orgId=1" + \
                 "&var-enterprise={}".format(site.Enterprise.EnterpriseId) + \
                 "&var-site={}".format(parameters["siteId"]) + \
@@ -50,7 +50,7 @@ def grafanaUrl(uid, parameters = None):
             elements = ""
             for element in elementTemplate.Elements:
                 elements += "&var-elements={}".format(element.ElementId)
-            return "http://{}:{}".format(current_app.config["GRAFANA_HOSTNAME"], current_app.config["GRAFANA_PORT"]) + \
+            return current_app.config["GRAFANA_BASE_URI"] + \
                 "/d/ElementSummary/element-summary?orgId=1" + \
                 "&var-enterprise={}".format(elementTemplate.Site.Enterprise.EnterpriseId) + \
                 "&var-site={}".format(elementTemplate.Site.SiteId) + \
@@ -59,7 +59,7 @@ def grafanaUrl(uid, parameters = None):
                 "&var-elementAttributeTemplates=All"
     elif uid == "ElementValuesGraph":
         element = Element.query.get_or_404(parameters["elementId"])
-        return "http://{}:{}".format(current_app.config["GRAFANA_HOSTNAME"], current_app.config["GRAFANA_PORT"]) + \
+        return current_app.config["GRAFANA_BASE_URI"] + \
             "/d/ElementValuesGraph/element-values-graph?orgId=1" + \
             "&var-enterprise={}".format(element.ElementTemplate.Site.Enterprise.EnterpriseId) + \
             "&var-site={}".format(element.ElementTemplate.Site.SiteId) + \
@@ -74,7 +74,7 @@ def grafanaUrl(uid, parameters = None):
         if eventFrame.EndTimestamp:
             endTimestamp = EventFrame.query.with_entities(func.unix_timestamp(EventFrame.EndTimestamp)).filter_by(EventFrameId = eventFrame.EventFrameId). \
                 one()[0]
-            return "http://{}:{}".format(current_app.config["GRAFANA_HOSTNAME"], current_app.config["GRAFANA_PORT"]) + \
+            return current_app.config["GRAFANA_BASE_URI"] + \
                 "/d/EventFramesGraph/event-frames-graph?orgId=1" + \
                 "&from={}000".format(startTimestamp) + \
                 "&to={}000".format(endTimestamp) + \
@@ -86,7 +86,7 @@ def grafanaUrl(uid, parameters = None):
                 "&var-eventFrameAttributeTemplates=All" + \
                 "&var-lookups=All"
         else:
-            return "http://{}:{}".format(current_app.config["GRAFANA_HOSTNAME"], current_app.config["GRAFANA_PORT"]) + \
+            return current_app.config["GRAFANA_BASE_URI"] + \
                 "/d/EventFramesGraph/event-frames-graph?orgId=1" + \
                 "&from={}000".format(startTimestamp) + \
                 "&to=now" + \
@@ -98,7 +98,7 @@ def grafanaUrl(uid, parameters = None):
                 "&var-eventFrameAttributeTemplates=All" + \
                 "&var-lookups=All"
     elif uid == "TagValuesGraph":
-        return "http://{}:{}".format(current_app.config["GRAFANA_HOSTNAME"], current_app.config["GRAFANA_PORT"]) + \
+        return current_app.config["GRAFANA_BASE_URI"] + \
             "/d/TagValuesGraph/tag-values-graph?orgId=1" + \
             "&var-enterprises=All" + \
             "&var-sites=All" + \
