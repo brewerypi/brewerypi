84523c0e375a29cac87904f0f3fc9b8f95ca23e3
Brian Faivre <brian.faivre@gmail.com>
2019-02-17 20:34:06 -0800

    Edit of Element Attribute Template.

--- app/elementAttributeTemplates/routes.py
+++ app/elementAttributeTemplates/routes.py
@@ -4,7 +4,7 @@ from . import elementAttributeTemplates
 from . forms import ElementAttributeTemplateForm
 from .. import db
 from .. decorators import adminRequired
-from .. models import ElementAttributeTemplate, ElementTemplate, Enterprise, Site
+from .. models import Area, ElementAttribute, ElementAttributeTemplate, ElementTemplate, Enterprise, Site, Tag
 
 @elementAttributeTemplates.route("/elementAttributeTemplates/add/<int:elementTemplateId>", methods = ["GET", "POST"])
 @elementAttributeTemplates.route("/elementAttributeTemplates/add/<int:elementTemplateId>/<int:lookup>", methods = ["GET", "POST"])
@@ -13,6 +13,7 @@ from .. models import ElementAttributeTemplate, ElementTemplate, Enterprise, Sit
 def addElementAttributeTemplate(elementTemplateId, lookup = False):
 	operation = "Add"
 	form = ElementAttributeTemplateForm()
+	elementTemplate = ElementTemplate.query.get_or_404(elementTemplateId)
 
 	if lookup:
 		modelName = "Lookup Element Attribute Template"
@@ -33,6 +34,30 @@ def addElementAttributeTemplate(elementTemplateId, lookup = False):
 		db.session.add(elementAttributeTemplate)
 		db.session.commit()
 		flash('You have successfully added the new element attribute template "{}".'.format(elementAttributeTemplate.Name), "alert alert-success")
+		for element in elementTemplate.Elements:
+			if element.isManaged():
+				# Tag management.
+				area = Area.query.get_or_404(element.TagAreaId)
+				tagName = "{}_{}".format(element.Name, elementAttributeTemplate.Name.replace(" ", ""))
+				tag = Tag.query.filter_by(AreaId = area.AreaId, Name = tagName).one_or_none()
+				if tag is None:
+					# Tag doesn't exist, so create it.
+					tag = Tag(AreaId = area.AreaId, LookupId = elementAttributeTemplate.LookupId, Name = tagName,
+						UnitOfMeasurementId = elementAttributeTemplate.UnitOfMeasurementId)
+					db.session.add(tag)
+				else:
+					# Tag exists, so update LookupId and UnitOfMeasurementId just in case.
+					tag.LookupId = elementAttributeTemplate.LookupId
+					tag.UnitOfMeasurementId = elementAttributeTemplate.UnitOfMeasurementId
+
+				db.session.commit()
+
+				# Element attribute management.
+				elementAttribute = ElementAttribute(ElementAttributeTemplateId = elementAttributeTemplate.ElementAttributeTemplateId, 
+					ElementId = element.ElementId, TagId = tag.TagId)
+				db.session.add(elementAttribute)
+				db.session.commit()
+
 		return redirect(form.requestReferrer.data)
 
 	# Present a form to add a new element attribute template.
@@ -40,7 +65,6 @@ def addElementAttributeTemplate(elementTemplateId, lookup = False):
 	if form.requestReferrer.data is None:
 		form.requestReferrer.data = request.referrer
 
-	elementTemplate = ElementTemplate.query.get_or_404(elementTemplateId)
 	breadcrumbs = [{"url" : url_for("elements.selectElement", selectedClass = "Root"),
 		"text" : "<span class = \"glyphicon glyphicon-home\"></span>"},
 		{"url" : url_for("elements.selectElement", selectedClass = "Enterprise", selectedId = elementTemplate.Site.Enterprise.EnterpriseId),
@@ -56,9 +80,18 @@ def addElementAttributeTemplate(elementTemplateId, lookup = False):
 @adminRequired
 def deleteElementAttributeTemplate(elementAttributeTemplateId):
 	elementAttributeTemplate = ElementAttributeTemplate.query.get_or_404(elementAttributeTemplateId)
+	tags = []
+	for elementAttribute in elementAttributeTemplate.ElementAttributes:
+		tags.append(elementAttribute.Tag)
+
 	elementAttributeTemplate.delete()
 	db.session.commit()
-	flash("You have successfully deleted the element attribute template \"{}\".".format(elementAttributeTemplate.Name), "alert alert-success")
+	flash('You have successfully deleted the element attribute template "{}".'.format(elementAttributeTemplate.Name), "alert alert-success")
+	for tag in tags:
+		if not tag.isReferenced():
+			tag.delete()
+	
+	db.session.commit()
 	return redirect(request.referrer)
 
 @elementAttributeTemplates.route("/elementAttributeTemplates/edit/<int:elementAttributeTemplateId>", methods = ["GET", "POST"])
@@ -78,6 +111,7 @@ def editElementAttributeTemplate(elementAttributeTemplateId):
 
 	# Edit an existing elementAttributeTemplate.
 	if form.validate_on_submit():
+		oldElementAttributeTemplateName = elementAttributeTemplate.Name
 		elementAttributeTemplate.Description = form.description.data
 		elementAttributeTemplate.ElementTemplateId = form.elementTemplateId.data
 		elementAttributeTemplate.Name = form.name.data
@@ -89,6 +123,50 @@ def editElementAttributeTemplate(elementAttributeTemplateId):
 
 		db.session.commit()
 		flash('You have successfully edited the element attribute template "{}".'.format(elementAttributeTemplate.Name), "alert alert-success")
+		for element in elementAttributeTemplate.ElementTemplate.Elements:
+			if element.isManaged():
+				# Tag management.
+				newTagName = "{}_{}".format(element.Name, elementAttributeTemplate.Name.replace(" ", ""))
+				oldTagName = "{}_{}".format(element.Name, oldElementAttributeTemplateName.replace(" ", ""))
+				oldTag = Tag.query.filter_by(AreaId = element.TagAreaId, Name = oldTagName).one_or_none()
+				if oldTag is None:
+					# Old tag doesn't exist.
+					newTag = Tag.query.filter_by(AreaId = element.TagAreaId, Name = newTagName).one_or_none()
+					if newTag is None:
+						# New tag doesn't exist, so create it.
+						tag = Tag(AreaId = element.TagAreaId, LookupId = elementAttributeTemplate.LookupId, Name = newTagName,
+							UnitOfMeasurementId = elementAttributeTemplate.UnitOfMeasurementId)
+						db.session.add(tag)
+						db.session.commit()
+						elementAttributeTagId = tag.TagId
+					else:
+						# New tag exists, so update LookupId and UnitOfMeasurementId just in case.
+						newTag.LookupId = elementAttributeTemplate.LookupId
+						newTag.UnitOfMeasurementId = elementAttributeTemplate.UnitOfMeasurementId
+						elementAttributeTagId = newTag.TagId
+						db.session.commit()
+				else:
+					# Old tag exists, so update LookupId, Name and UnitOfMeasurementId just in case.
+					oldTag.LookupId = elementAttributeTemplate.LookupId
+					oldTag.Name = newTagName
+					oldTag.UnitOfMeasurementId = elementAttributeTemplate.UnitOfMeasurementId
+					elementAttributeTagId = oldTag.TagId
+					db.session.commit()
+
+				# Element attribute management.
+				elementAttribute = ElementAttribute.query.filter_by(ElementAttributeTemplateId = elementAttributeTemplate.ElementAttributeTemplateId,
+					ElementId = element.ElementId).one_or_none()
+				if elementAttribute is None:
+					# The element attribute doesn't exist, so create it.
+					elementAttribute = ElementAttribute(ElementAttributeTemplateId = elementAttributeTemplate.ElementAttributeTemplateId, 
+						ElementId = element.ElementId, TagId = elementAttributeTagId)
+					db.session.add(elementAttribute)
+				else:
+					# The element attribute exists, so update TagId just in case.
+					elementAttribute.TagId = elementAttributeTagId
+
+				db.session.commit()
+
 		return redirect(form.requestReferrer.data)
 
 	# Present a form to edit an existing elementAttributeTemplate.
