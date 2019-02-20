baeffcc036abf469eb76d709e70fb149fe700c4a
Brian Faivre <brian.faivre@gmail.com>
2019-02-20 08:36:39 -0800

    Added flash messaging to EATs add, delete and edit.

--- app/elementAttributeTemplates/routes.py
+++ app/elementAttributeTemplates/routes.py
@@ -24,6 +24,9 @@ def addElementAttributeTemplate(elementTemplateId, lookup = False):
 
 	# Add a new elementAttributeTemplate.
 	if form.validate_on_submit():
+		createdTags = []
+		updatedTags = []
+		createdElementAttributes = []
 		if lookup:
 			elementAttributeTemplate = ElementAttributeTemplate(Description = form.description.data, ElementTemplateId = form.elementTemplateId.data, \
 				Lookup = form.lookup.data, Name = form.name.data)
@@ -45,10 +48,12 @@ def addElementAttributeTemplate(elementTemplateId, lookup = False):
 					tag = Tag(AreaId = area.AreaId, LookupId = elementAttributeTemplate.LookupId, Name = tagName,
 						UnitOfMeasurementId = elementAttributeTemplate.UnitOfMeasurementId)
 					db.session.add(tag)
+					createdTags.append(tag)
 				else:
 					# Tag exists, so update LookupId and UnitOfMeasurementId just in case.
 					tag.LookupId = elementAttributeTemplate.LookupId
 					tag.UnitOfMeasurementId = elementAttributeTemplate.UnitOfMeasurementId
+					updatedTags.append(tag)
 
 				db.session.commit()
 
@@ -57,6 +62,45 @@ def addElementAttributeTemplate(elementTemplateId, lookup = False):
 					ElementId = element.ElementId, TagId = tag.TagId)
 				db.session.add(elementAttribute)
 				db.session.commit()
+				createdElementAttributes.append(elementAttribute)
+
+		createdTagsMessage = ""
+		if createdTags:
+			createdTags.sort(key = lambda tag: tag.Name)
+			for tag in createdTags:
+				if createdTagsMessage == "":
+					createdTagsMessage = 'Created the following tag(s):<br>"{}"'.format(tag.Name)
+					alert = "alert alert-success"
+				else:
+					createdTagsMessage = '{}<br>"{}"'.format(createdTagsMessage, tag.Name)
+
+			flash(createdTagsMessage, alert)
+
+		updatedTagsMessage = ""
+		if updatedTags:
+			updatedTags.sort(key = lambda tag: tag.Name)
+			for tag in updatedTags:
+				if updatedTagsMessage == "":
+					updatedTagsMessage = 'Updated the following existing tag(s), if needed:<br>"{}"'.format(tag.Name)
+					alert = "alert alert-warning"
+				else:
+					updatedTagsMessage = '{}<br>"{}"'.format(updatedTagsMessage, tag.Name)
+
+			flash(updatedTagsMessage, alert)
+
+		createdElementAttributesMessage = ""
+		if createdElementAttributes:
+			createdElementAttributes.sort(key = lambda tag: tag.Element.Name)
+			for elementAttribute in createdElementAttributes:
+				if createdElementAttributesMessage == "":
+					createdElementAttributesMessage = 'Created the following element attribute(s):<br>Element: "{}" attribute: "{}" associated with tag: "{}"'. \
+						format(elementAttribute.Element.Name, elementAttribute.ElementAttributeTemplate.Name, elementAttribute.Tag.Name)
+					alert = "alert alert-success"
+				else:
+					createdElementAttributesMessage = '{}<br>Element: "{}" attribute: "{}" associated with tag: "{}"'.format(createdElementAttributesMessage,
+						elementAttribute.Element.Name, elementAttribute.ElementAttributeTemplate.Name, elementAttribute.Tag.Name)
+
+			flash(createdElementAttributesMessage, alert)
 
 		return redirect(form.requestReferrer.data)
 
@@ -87,11 +131,20 @@ def deleteElementAttributeTemplate(elementAttributeTemplateId):
 	elementAttributeTemplate.delete()
 	db.session.commit()
 	flash('You have successfully deleted the element attribute template "{}".'.format(elementAttributeTemplate.Name), "alert alert-success")
+	deletedTagsMessage = ""
+	tags.sort(key = lambda tag: tag.Name)
 	for tag in tags:
 		if not tag.isReferenced():
 			tag.delete()
-	
+			if deletedTagsMessage == "":
+				deletedTagsMessage = 'Deleted the following tag(s):<br>"{}"'.format(tag.Name)
+			else:
+				deletedTagsMessage = '{}<br>"{}"'.format(deletedTagsMessage, tag.Name)
+
 	db.session.commit()
+	if deletedTagsMessage != "":
+		alert = "alert alert-success"
+		flash(deletedTagsMessage, alert)
 	return redirect(request.referrer)
 
 @elementAttributeTemplates.route("/elementAttributeTemplates/edit/<int:elementAttributeTemplateId>", methods = ["GET", "POST"])
@@ -111,6 +164,10 @@ def editElementAttributeTemplate(elementAttributeTemplateId):
 
 	# Edit an existing elementAttributeTemplate.
 	if form.validate_on_submit():
+		createdTags = []
+		updatedTags = []
+		createdElementAttributes = []
+		updatedElementAttributes = []
 		oldElementAttributeTemplateName = elementAttributeTemplate.Name
 		elementAttributeTemplate.Description = form.description.data
 		elementAttributeTemplate.ElementTemplateId = form.elementTemplateId.data
@@ -138,20 +195,23 @@ def editElementAttributeTemplate(elementAttributeTemplateId):
 							UnitOfMeasurementId = elementAttributeTemplate.UnitOfMeasurementId)
 						db.session.add(tag)
 						db.session.commit()
+						createdTags.append(tag)
 						elementAttributeTagId = tag.TagId
 					else:
 						# New tag exists, so update LookupId and UnitOfMeasurementId just in case.
 						newTag.LookupId = elementAttributeTemplate.LookupId
 						newTag.UnitOfMeasurementId = elementAttributeTemplate.UnitOfMeasurementId
-						elementAttributeTagId = newTag.TagId
 						db.session.commit()
+						updatedTags.append(newTag)
+						elementAttributeTagId = newTag.TagId
 				else:
 					# Old tag exists, so update LookupId, Name and UnitOfMeasurementId just in case.
 					oldTag.LookupId = elementAttributeTemplate.LookupId
 					oldTag.Name = newTagName
 					oldTag.UnitOfMeasurementId = elementAttributeTemplate.UnitOfMeasurementId
-					elementAttributeTagId = oldTag.TagId
 					db.session.commit()
+					updatedTags.append(oldTag)
+					elementAttributeTagId = oldTag.TagId
 
 				# Element attribute management.
 				elementAttribute = ElementAttribute.query.filter_by(ElementAttributeTemplateId = elementAttributeTemplate.ElementAttributeTemplateId,
@@ -161,11 +221,68 @@ def editElementAttributeTemplate(elementAttributeTemplateId):
 					elementAttribute = ElementAttribute(ElementAttributeTemplateId = elementAttributeTemplate.ElementAttributeTemplateId, 
 						ElementId = element.ElementId, TagId = elementAttributeTagId)
 					db.session.add(elementAttribute)
+					db.session.commit()
+					createdElementAttributes.append(elementAttribute)
 				else:
 					# The element attribute exists, so update TagId just in case.
 					elementAttribute.TagId = elementAttributeTagId
+					db.session.commit()
+					updatedElementAttributes.append(elementAttribute)
 
-				db.session.commit()
+
+		createdTagsMessage = ""
+		alert = "alert alert-success"
+		if createdTags:
+			createdTags.sort(key = lambda tag: tag.Name)
+			for tag in createdTags:
+				if createdTagsMessage == "":
+					createdTagsMessage = 'Created the following tag(s):<br>"{}"'.format(tag.Name)
+					alert = "alert alert-success"
+				else:
+					createdTagsMessage = '{}<br>"{}"'.format(createdTagsMessage, tag.Name)
+
+			flash(createdTagsMessage, alert)
+
+		updatedTagsMessage = ""
+		if updatedTags:
+			updatedTags.sort(key = lambda tag: tag.Name)
+			for tag in updatedTags:
+				if updatedTagsMessage == "":
+					updatedTagsMessage = 'Updated the following existing tag(s), if needed:<br>"{}"'.format(tag.Name)
+					alert = "alert alert-warning"
+				else:
+					updatedTagsMessage = '{}<br>"{}"'.format(updatedTagsMessage, tag.Name)
+
+			flash(updatedTagsMessage, alert)
+
+		createdElementAttributesMessage = ""
+		if createdElementAttributes:
+			createdElementAttributes.sort(key = lambda elementAttribute: elementAttribute.Element.Name)
+			for elementAttribute in createdElementAttributes:
+				if createdElementAttributesMessage == "":
+					createdElementAttributesMessage = 'Created the following element attribute(s):<br>Element: "{}" attribute: "{}" associated with tag: "{}"'. \
+						format(elementAttribute.Element.Name, elementAttribute.ElementAttributeTemplate.Name, elementAttribute.Tag.Name)
+					alert = "alert alert-success"
+				else:
+					createdElementAttributesMessage = '{}<br>Element: "{}" attribute: "{}" associated with tag: "{}"'.format(createdElementAttributesMessage,
+						elementAttribute.Element.Name, elementAttribute.ElementAttributeTemplate.Name, elementAttribute.Tag.Name)
+
+			flash(createdElementAttributesMessage, alert)
+
+		updatedElementAttributesMessage = ""
+		if updatedElementAttributes:
+			updatedElementAttributes.sort(key = lambda elementAttribute: elementAttribute.Element.Name)
+			for elementAttribute in updatedElementAttributes:
+				if updatedElementAttributesMessage == "":
+					updatedElementAttributesMessage = "Updated the following element attribute(s), if needed:<br>Element: " + \
+						'"{}" attribute: "{}" associated with tag: "{}"'.format(elementAttribute.Element.Name, elementAttribute.ElementAttributeTemplate.Name,
+						elementAttribute.Tag.Name)
+					alert = "alert alert-warning"
+				else:
+					updatedElementAttributesMessage = '{}<br>Element: "{}" attribute: "{}" associated with tag: "{}"'.format(updatedElementAttributesMessage,
+						elementAttribute.Element.Name, elementAttribute.ElementAttributeTemplate.Name, elementAttribute.Tag.Name)
+
+			flash(updatedElementAttributesMessage, alert)
 
 		return redirect(form.requestReferrer.data)
 
