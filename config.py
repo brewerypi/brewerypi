d329e82faeb1533d019cd08f94dd27d26dd6fd26
Brian Faivre <brian.faivre@gmail.com>
2019-04-25 08:13:37 -0700

    Notifications interval from .env.

--- config.py
+++ config.py
@@ -20,6 +20,7 @@ class Config:
 	IS_RASPBERRY_PI = True if os.environ.get("IS_RASPBERRY_PI") == "1" else False
 	LOCAL_TIMEZONE = os.environ.get("LOCAL_TIMEZONE")
 	MYSQL_DATABASE = os.environ.get("MYSQL_DATABASE") or "BreweryPi"
+	NOTIFICATIONS_INTERVAL_IN_MILLISECONDS = os.environ.get("NOTIFICATIONS_INTERVAL") or 10000
 	SECRET_KEY = os.environ.get("SECRET_KEY") or "Replace with a hard to guess string."
 	SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI") or "mysql://pi:brewery@localhost/BreweryPi"
 	SQLALCHEMY_SERVER_URI = os.environ.get("SQLALCHEMY_SERVER_URI") or "mysql://pi:brewery@localhost"
