5be7adbfb20f467e29fca89aac4d79342472e02f
Brian Faivre <brian.faivre@gmail.com>
2019-05-20 23:12:46 -0700

    Removed multi-select verbage as browser specific.

--- app/messages/forms.py
+++ app/messages/forms.py
@@ -4,7 +4,7 @@ from wtforms.validators import DataRequired, Required
 from .. models import User
 
 class MessageForm(FlaskForm):
-	recipient = SelectMultipleField("To (Use Ctrl key to select multiple, Ctrl+a to select all)", validators = [Required()], coerce = int)
+	recipient = SelectMultipleField("To", validators = [Required()], coerce = int)
 	body = TextAreaField("Message", validators = [DataRequired()])
 	requestReferrer = HiddenField()
 	submit = SubmitField("Save")
