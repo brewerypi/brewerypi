7e9eaabcf10681e44891683fb92008f3d797d162
Brian Faivre <brian.faivre@gmail.com>
2019-12-08 22:21:59 -0800

    Breadcrumb for EFG tag value notes.

--- app/tagValueNotes/routes.py
+++ app/tagValueNotes/routes.py
@@ -5,36 +5,41 @@ from . import tagValueNotes
 from . forms import TagValueNoteForm
 from .. import db
 from .. decorators import permissionRequired
-from .. models import ElementAttribute, EventFrame, EventFrameAttribute, Note, Permission, TagValue, TagValueNote
+from .. models import ElementAttribute, EventFrame, EventFrameAttribute, EventFrameGroup, Note, Permission, TagValue, TagValueNote
 
 modelName = "Tag Value Note"
 
 @tagValueNotes.route("/tagValueNotes/<int:tagValueId>", methods = ["GET", "POST"])
 @tagValueNotes.route("/tagValueNotes/<int:tagValueId>/<int:elementAttributeId>", methods = ["GET", "POST"])
 @tagValueNotes.route("/tagValueNotes/<int:tagValueId>/<int:eventFrameId>/<int:eventFrameAttributeId>", methods = ["GET", "POST"])
+@tagValueNotes.route("/tagValueNotes/<int:tagValueId>/<int:eventFrameId>/<int:eventFrameAttributeId>/<int:eventFrameGroupId>", methods = ["GET", "POST"])
 @login_required
 @permissionRequired(Permission.DATA_ENTRY)
-def listTagValueNotes(tagValueId, elementAttributeId = None, eventFrameId = None, eventFrameAttributeId = None):
+def listTagValueNotes(tagValueId, elementAttributeId = None, eventFrameId = None, eventFrameAttributeId = None, eventFrameGroupId = None):
 	elementAttribute = None
 	eventFrame = None
 	eventFrameAttribute = None
+	eventFrameGroup = None
 	if elementAttributeId:
 		elementAttribute = ElementAttribute.query.get_or_404(elementAttributeId)
 	elif eventFrameId:
 		eventFrame = EventFrame.query.get_or_404(eventFrameId)
 		eventFrameAttribute = EventFrameAttribute.query.get_or_404(eventFrameAttributeId)
+		if eventFrameGroupId:
+			eventFrameGroup = EventFrameGroup.query.get_or_404(eventFrameGroupId)
 
 	tagValue = TagValue.query.get_or_404(tagValueId)
 	tagValueNotes = TagValueNote.query.filter_by(TagValueId = tagValueId)
 	return render_template("tagValueNotes/tagValueNotes.html", elementAttribute = elementAttribute, eventFrame = eventFrame,
-		eventFrameAttribute = eventFrameAttribute, tagValue = tagValue, tagValueNotes = tagValueNotes)
+		eventFrameAttribute = eventFrameAttribute, eventFrameGroup = eventFrameGroup, tagValue = tagValue, tagValueNotes = tagValueNotes)
 
 @tagValueNotes.route("/tagValueNotes/add/<int:tagValueId>", methods = ["GET", "POST"])
 @tagValueNotes.route("/tagValueNotes/add/<int:tagValueId>/<int:elementAttributeId>", methods = ["GET", "POST"])
 @tagValueNotes.route("/tagValueNotes/add/<int:tagValueId>/<int:eventFrameId>/<int:eventFrameAttributeId>", methods = ["GET", "POST"])
+@tagValueNotes.route("/tagValueNotes/add/<int:tagValueId>/<int:eventFrameId>/<int:eventFrameAttributeId>/<int:eventFrameGroupId>", methods = ["GET", "POST"])
 @login_required
 @permissionRequired(Permission.DATA_ENTRY)
-def addTagValueNote(tagValueId, elementAttributeId = None, eventFrameId = None, eventFrameAttributeId = None):
+def addTagValueNote(tagValueId, elementAttributeId = None, eventFrameId = None, eventFrameAttributeId = None, eventFrameGroupId = None):
 	operation = "Add"
 	modelName = "Tag Value Note"
 	form = TagValueNoteForm()
@@ -70,6 +75,27 @@ def addTagValueNote(tagValueId, elementAttributeId = None, eventFrameId = None, 
 				"text" : elementAttribute.ElementAttributeTemplate.Name},
 			{"url" : url_for("tagValueNotes.listTagValueNotes", tagValueId = tagValue.TagValueId, elementAttributeId = elementAttribute.ElementAttributeId),
 				"text" : tagValue.Timestamp, "type" : "timestamp"}]
+	elif eventFrameGroupId:
+		eventFrame = EventFrame.query.get_or_404(eventFrameId)
+		eventFrameAttribute = EventFrameAttribute.query.get_or_404(eventFrameAttributeId)
+		eventFrameGroup = EventFrameGroup.query.get_or_404(eventFrameGroupId)
+		breadcrumbs = [{"url": url_for("eventFrameGroups.listEventFrameGroups"), "text": "<span class = \"glyphicon glyphicon-home\"></span>"},
+			{"url": url_for("eventFrameGroups.dashboard", eventFrameGroupId = eventFrameGroup.EventFrameGroupId), "text": eventFrameGroup.Name},
+			{"url": url_for("eventFrames.dashboard", eventFrameId = eventFrame.origin().EventFrameId,
+				eventFrameGroupId = eventFrameGroup.EventFrameGroupId), "text": eventFrame.origin().Name}]
+		eventFrames = eventFrame.ancestors([])
+		eventFrames.append(eventFrame)
+		for eventFrame in eventFrames:
+			if eventFrame.ParentEventFrameId is not None:
+				breadcrumbs.append({"url": url_for("eventFrames.dashboard", eventFrameId = eventFrame.EventFrameId,
+					eventFrameGroupId = eventFrameGroup.EventFrameGroupId),
+					"text": "{} / {}".format(eventFrame.EventFrameTemplate.Name, eventFrame.Name)})
+
+		breadcrumbs.append({"url": url_for("tagValues.listTagValues", tagValueId = tagValue.TagValueId, eventFrameId = eventFrame.EventFrameId,
+			eventFrameAttributeId = eventFrameAttributeId, eventFrameGroupId = eventFrameGroup.EventFrameGroupId),
+			"text": eventFrameAttribute.EventFrameAttributeTemplate.Name})
+		breadcrumbs.append({"url" : url_for("tagValueNotes.listTagValueNotes", tagValueId = tagValue.TagValueId, eventFrameId = eventFrame.EventFrameId,
+			eventFrameAttributeId = eventFrameAttributeId, eventFrameGroupId = eventFrameGroup.EventFrameGroupId), "text" : tagValue.Timestamp})
 	elif eventFrameId:
 		eventFrame = EventFrame.query.get_or_404(eventFrameId)
 		eventFrameAttribute = EventFrameAttribute.query.get_or_404(eventFrameAttributeId)
@@ -113,9 +139,11 @@ def deleteTagValueNote(noteId, tagValueId):
 @tagValueNotes.route("/tagValueNotes/edit/<int:noteId>/<int:tagValueId>", methods = ["GET", "POST"])
 @tagValueNotes.route("/tagValueNotes/edit/<int:noteId>/<int:tagValueId>/<int:elementAttributeId>", methods = ["GET", "POST"])
 @tagValueNotes.route("/tagValueNotes/edit/<int:noteId>/<int:tagValueId>/<int:eventFrameId>/<int:eventFrameAttributeId>", methods = ["GET", "POST"])
+@tagValueNotes.route("/tagValueNotes/edit/<int:noteId>/<int:tagValueId>/<int:eventFrameId>/<int:eventFrameAttributeId>/<int:eventFrameGroupId>",
+	methods = ["GET", "POST"])
 @login_required
 @permissionRequired(Permission.DATA_ENTRY)
-def editTagValueNote(noteId, tagValueId, elementAttributeId = None, eventFrameId = None, eventFrameAttributeId = None):
+def editTagValueNote(noteId, tagValueId, elementAttributeId = None, eventFrameId = None, eventFrameAttributeId = None, eventFrameGroupId = None):
 	operation = "Edit"
 	modelName = "Tag Value Note"
 	note = Note.query.get_or_404(noteId)
@@ -153,6 +181,27 @@ def editTagValueNote(noteId, tagValueId, elementAttributeId = None, eventFrameId
 			{"url" : url_for("tagValueNotes.listTagValueNotes", tagValueId = tagValue.TagValueId, elementAttributeId = elementAttribute.ElementAttributeId),
 				"text" : tagValue.Timestamp, "type" : "timestamp"},
 			{"url" : None, "text" : note.Timestamp, "type" : "timestamp"}]
+	elif eventFrameGroupId:
+		eventFrame = EventFrame.query.get_or_404(eventFrameId)
+		eventFrameAttribute = EventFrameAttribute.query.get_or_404(eventFrameAttributeId)
+		eventFrameGroup = EventFrameGroup.query.get_or_404(eventFrameGroupId)
+		breadcrumbs = [{"url": url_for("eventFrameGroups.listEventFrameGroups"), "text": "<span class = \"glyphicon glyphicon-home\"></span>"},
+			{"url": url_for("eventFrameGroups.dashboard", eventFrameGroupId = eventFrameGroup.EventFrameGroupId), "text": eventFrameGroup.Name},
+			{"url": url_for("eventFrames.dashboard", eventFrameId = eventFrame.origin().EventFrameId,
+				eventFrameGroupId = eventFrameGroup.EventFrameGroupId), "text": eventFrame.origin().Name}]
+		eventFrames = eventFrame.ancestors([])
+		eventFrames.append(eventFrame)
+		for eventFrame in eventFrames:
+			if eventFrame.ParentEventFrameId is not None:
+				breadcrumbs.append({"url": url_for("eventFrames.dashboard", eventFrameId = eventFrame.EventFrameId,
+					eventFrameGroupId = eventFrameGroup.EventFrameGroupId),
+					"text": "{} / {}".format(eventFrame.EventFrameTemplate.Name, eventFrame.Name)})
+
+		breadcrumbs.append({"url": url_for("tagValues.listTagValues", tagValueId = tagValue.TagValueId, eventFrameId = eventFrame.EventFrameId,
+			eventFrameAttributeId = eventFrameAttributeId, eventFrameGroupId = eventFrameGroup.EventFrameGroupId),
+			"text": eventFrameAttribute.EventFrameAttributeTemplate.Name})
+		breadcrumbs.append({"url" : url_for("tagValueNotes.listTagValueNotes", tagValueId = tagValue.TagValueId, eventFrameId = eventFrame.EventFrameId,
+			eventFrameAttributeId = eventFrameAttributeId, eventFrameGroupId = eventFrameGroup.EventFrameGroupId), "text" : tagValue.Timestamp})
 	elif eventFrameId:
 		eventFrame = EventFrame.query.get_or_404(eventFrameId)
 		eventFrameAttribute = EventFrameAttribute.query.get_or_404(eventFrameAttributeId)
