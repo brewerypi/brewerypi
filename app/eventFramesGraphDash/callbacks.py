880238906a40908cc220104e23db418c6d0952d0
Brian Faivre <brian.faivre@gmail.com>
2020-05-14 13:39:58 -0700

    Added dropdown navigation.

--- app/eventFramesGraphDash/callbacks.py
+++ app/eventFramesGraphDash/callbacks.py
@@ -1,16 +1,69 @@
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 from dash.exceptions import PreventUpdate
-from app.models import EventFrame, EventFrameTemplateView
+from urllib.parse import parse_qs, urlparse
+from app.models import ElementTemplate, Enterprise, EventFrame, EventFrameTemplate, EventFrameTemplateView, Site
 
 def registerCallbacks(dashApp):
-    @dashApp.callback(Output(component_id = "eventFrameDropdown", component_property = "options"),
+    @dashApp.callback(Output(component_id = "enterpriseDropdown", component_property = "options"),
         [Input(component_id = "url", component_property = "href")])
-    def eventFrameDropdown(urlHref):
-        return [{"label": eventFrame.Name, "value": eventFrame.EventFrameId} for eventFrame in
-            EventFrame.query.order_by(EventFrame.StartTimestamp.desc()).all()]
+    def enterpriseDropDownOptions(urlHref):
+        return [{"label": enterprise.Name, "value": enterprise.EnterpriseId} for enterprise in Enterprise.query.order_by(Enterprise.Name).all()]
 
-    @dashApp.callback([Output(component_id = "graph", component_property = "figure"),
-        Output(component_id = "eventFrameTemplateViewDropdown", component_property = "options")],
+    @dashApp.callback(Output(component_id = "enterpriseDropdown", component_property = "value"),
+        [Input(component_id = "enterpriseDropdown", component_property = "options"),
+        Input(component_id = "url", component_property = "href")])
+    def enterpriseDropdownValue(enterpriseDropdownOptions, urlHref):
+        if enterpriseDropdownOptions is None:
+            raise PreventUpdate
+        else:
+            queryString = parse_qs(urlparse(urlHref).query)
+            if "enterpriseId" in queryString:
+                return int(queryString["enterpriseId"][0])
+            else:
+                return enterpriseDropdownOptions[0]["value"]
+
+    @dashApp.callback([Output(component_id = "siteDropdown", component_property = "options"),
+        Output(component_id = "siteDropdown", component_property = "value")],
+        [Input(component_id = "enterpriseDropdown", component_property = "value")])
+    def siteDropdownOptions(enterpriseDropdownValue):
+        return [[{"label": site.Name, "value": site.SiteId} for site in Site.query.filter_by(EnterpriseId = enterpriseDropdownValue). \
+            order_by(Site.Name).all()], None]
+
+    @dashApp.callback([Output(component_id = "elementTemplateDropdown", component_property = "options"),
+        Output(component_id = "elementTemplateDropdown", component_property = "value")],
+        [Input(component_id = "siteDropdown", component_property = "value")])
+    def elementTemplateDropdownOptions(siteDropdownValue):
+        return [[{"label": elementTemplate.Name, "value": elementTemplate.ElementTemplateId} for elementTemplate in ElementTemplate.query. \
+            filter_by(SiteId = siteDropdownValue).order_by(ElementTemplate.Name).all()], None]
+
+    @dashApp.callback([Output(component_id = "eventFrameTemplateDropdown", component_property = "options"),
+        Output(component_id = "eventFrameTemplateDropdown", component_property = "value")],
+        [Input(component_id = "elementTemplateDropdown", component_property = "value")])
+    def eventFrameTemplateDropdownOptions(elementTemplateDropdownValue):
+        return [[{"label": eventFrameTemplate.Name, "value": eventFrameTemplate.EventFrameTemplateId} for eventFrameTemplate in EventFrameTemplate.query. \
+            filter_by(ElementTemplateId = elementTemplateDropdownValue).order_by(EventFrameTemplate.Name).all()], None]
+
+    @dashApp.callback([Output(component_id = "eventFrameDropdown", component_property = "options"),
+        Output(component_id = "eventFrameDropdown", component_property = "value")],
+        [Input(component_id = "eventFrameTemplateDropdown", component_property = "value")])
+    def eventFrameDropdownOptions(eventFrameTemplateDropdownValue):
+        return [[{"label": eventFrame.Name, "value": eventFrame.EventFrameId} for eventFrame in EventFrame.query. \
+            filter_by(EventFrameTemplateId = eventFrameTemplateDropdownValue).order_by(EventFrame.StartTimestamp.desc()).all()], None]
+
+    @dashApp.callback([Output(component_id = "eventFrameTemplateViewDropdown", component_property = "options"),
+        Output(component_id = "eventFrameTemplateViewDropdown", component_property = "value")],
+        [Input(component_id = "eventFrameTemplateDropdown", component_property = "value")])
+    def eventFrameDropdownOptions(eventFrameTemplateDropdownValue):
+        if eventFrameTemplateDropdownValue is None:
+            return [[], None]
+
+        eventFrameTemplateViews = [{"label": eventFrameTemplateView.Name, "value": eventFrameTemplateView.EventFrameTemplateViewId}
+            for eventFrameTemplateView in EventFrameTemplateView.query.filter_by(EventFrameTemplateId = eventFrameTemplateDropdownValue). \
+                order_by(EventFrameTemplateView.Name).all()]
+        eventFrameTemplateViews.insert(0, {"label": "All", "value": -1})
+        return [eventFrameTemplateViews, -1]
+
+    @dashApp.callback(Output(component_id = "graph", component_property = "figure"),
         [Input(component_id = "eventFrameDropdown", component_property = "value"),
         Input(component_id = "eventFrameTemplateViewDropdown", component_property = "value")])
     def graphFigure(eventFrameDropdownValue, eventFrameTemplateViewDropdownValue):
@@ -34,8 +87,4 @@ def registerCallbacks(dashApp):
                 seriesDict["x"].append(tagValue.Timestamp)
                 seriesDict["y"].append(tagValue.Value)
 
-        eventFrameTemplateViews = [{"label": eventFrameTemplateView.Name, "value": eventFrameTemplateView.EventFrameTemplateViewId}
-            for eventFrameTemplateView in EventFrameTemplateView.query.filter_by(EventFrameTemplateId = eventFrame.EventFrameTemplateId). \
-                order_by(EventFrameTemplateView.Name).all()]
-        eventFrameTemplateViews.insert(0, {"label": "All", "value": -1})
-        return [{"data": data}, eventFrameTemplateViews]
+        return {"data": data}
--- app/eventFramesGraphDash/layout.py
+++ app/eventFramesGraphDash/layout.py
@@ -1,13 +1,19 @@
 import dash_core_components as dcc
 import dash_html_components as html
 
-layout = html.Div \
-(
-    children =
+layout = html.Div(children =
+[
+    dcc.Location(id = "url"),
+    dcc.Interval(id = "interval", n_intervals = 0, disabled = True),
+    html.Div(className = "page-header", children = [html.H1("Event Frames Graph")]),
+    html.Div(children =
     [
-        dcc.Location(id = "url"),
+        dcc.Dropdown(id = "enterpriseDropdown", placeholder = "Select Enterprise", multi = False),
+        dcc.Dropdown(id = "siteDropdown", placeholder = "Select Site", multi = False),
+        dcc.Dropdown(id = "elementTemplateDropdown", placeholder = "Select Element Template", multi = False),
+        dcc.Dropdown(id = "eventFrameTemplateDropdown", placeholder = "Select Event Frame Template", multi = False),
         dcc.Dropdown(id = "eventFrameDropdown", placeholder = "Select Event Frame", multi = False),
-        dcc.Dropdown(id = "eventFrameTemplateViewDropdown", placeholder = "Select View", multi = False),
-        dcc.Graph(id = "graph")
-    ]
-)
+        dcc.Dropdown(id = "eventFrameTemplateViewDropdown", placeholder = "Select View", multi = False)
+    ]),
+    dcc.Graph(id = "graph")
+])
