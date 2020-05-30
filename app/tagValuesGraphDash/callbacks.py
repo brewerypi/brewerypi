16881be8271f649362305fd7accbd38ef4e13890
Brian Faivre <brian.faivre@gmail.com>
2020-05-30 14:06:15 -0700

    Whitespace cleanup.

--- app/tagValuesGraphDash/callbacks.py
+++ app/tagValuesGraphDash/callbacks.py
@@ -237,43 +237,43 @@ def registerCallbacks(dashApp):
             refreshRateText = "Off"
             disabled = True
         elif "fiveSecondLi" in changedId:
-            refreshRateText = "5s "
+            refreshRateText = "5s"
             refreshRateSeconds = 1000 * 5
             disabled = False
         elif "tenSecondLi" in changedId:
-            refreshRateText = "10s "
+            refreshRateText = "10s"
             refreshRateSeconds = 1000 * 10
             disabled = False
         elif "thirtySecondLi" in changedId:
-            refreshRateText = "30s "
+            refreshRateText = "30s"
             refreshRateSeconds = 1000 * 30
             disabled = False
         elif "oneMinuteLi" in changedId:
-            refreshRateText = "1m "
+            refreshRateText = "1m"
             refreshRateSeconds = 1000 * 60
             disabled = False
         elif "fiveMinuteLi" in changedId:
-            refreshRateText = "5m "
+            refreshRateText = "5m"
             refreshRateSeconds = 1000 * 60 * 5
             disabled = False
         elif "fifthteenMinuteLi" in changedId:
-            refreshRateText = "15m "
+            refreshRateText = "15m"
             refreshRateSeconds = 1000 * 60 * 15
             disabled = False
         elif "thirtyMinuteLi" in changedId:
-            refreshRateText = "30m "
+            refreshRateText = "30m"
             refreshRateSeconds = 1000 * 60 * 30
             disabled = False
         elif "oneHourLi" in changedId:
-            refreshRateText = "1h "
+            refreshRateText = "1h"
             refreshRateSeconds = 1000 * 60 * 60
             disabled = False
         elif "twoHourLi" in changedId:
-            refreshRateText = "2h "
+            refreshRateText = "2h"
             refreshRateSeconds = 1000 * 60 * 60 * 2
             disabled = False
         elif "oneDayLi" in changedId:
-            refreshRateText = "1d "
+            refreshRateText = "1d"
             refreshRateSeconds = 1000 * 60 * 60 * 24
             disabled = False
         else:
@@ -281,4 +281,4 @@ def registerCallbacks(dashApp):
             refreshRateSeconds = 1000
             disabled = True
 
-        return [refreshRateText, html.Span(className = "caret")], refreshRateSeconds, disabled
+        return [refreshRateText + " ", html.Span(className = "caret")], refreshRateSeconds, disabled
