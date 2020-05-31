9a41e3bc24dc8498d3f6f17907cb88564733eb65
Brian Faivre <brian.faivre@gmail.com>
2020-05-30 23:30:17 -0700

    Dashes helpers starting w/shared interval().

--- app/eventFrameGraphDash/callbacks.py
+++ app/eventFrameGraphDash/callbacks.py
@@ -1,5 +1,4 @@
 import dash
-import dash_html_components as html
 import pytz
 from dash.dependencies import Input, Output, State
 from dash.exceptions import PreventUpdate
@@ -8,6 +7,7 @@ from dateutil.relativedelta import relativedelta
 from sqlalchemy import or_
 from urllib.parse import parse_qs, urlparse
 from app.models import ElementTemplate, Enterprise, EventFrame, EventFrameNote, EventFrameTemplate, EventFrameTemplateView, LookupValue, Note, Site
+from app.dashes import helpers
 
 def registerCallbacks(dashApp):
     @dashApp.callback(Output(component_id = "fromTimestampInput", component_property = "value"),
@@ -325,55 +325,5 @@ def registerCallbacks(dashApp):
         Input(component_id = "oneHourLi", component_property = "n_clicks"),
         Input(component_id = "twoHourLi", component_property = "n_clicks"),
         Input(component_id = "oneDayLi", component_property = "n_clicks")])
-    def interval(offLiNClicks, fiveSecondLiNClicks, tenSecondLiNClicks, thirtySecondLiNClicks, oneMinuteLiNClicks, fiveMinuteLiNClicks,
-        fifthteenMinuteLiNClicks, thirtyMinuteLiNClicks, oneHourLiNClicks, twoHourLiNClicks, oneDayLiNClicks):
-        changedId = [property['prop_id'] for property in dash.callback_context.triggered][0]
-        if "offLiNClicks" in changedId:
-            refreshRateText = "Off"
-            disabled = True
-        elif "fiveSecondLi" in changedId:
-            refreshRateText = "5s"
-            refreshRateSeconds = 1000 * 5
-            disabled = False
-        elif "tenSecondLi" in changedId:
-            refreshRateText = "10s"
-            refreshRateSeconds = 1000 * 10
-            disabled = False
-        elif "thirtySecondLi" in changedId:
-            refreshRateText = "30s"
-            refreshRateSeconds = 1000 * 30
-            disabled = False
-        elif "oneMinuteLi" in changedId:
-            refreshRateText = "1m"
-            refreshRateSeconds = 1000 * 60
-            disabled = False
-        elif "fiveMinuteLi" in changedId:
-            refreshRateText = "5m"
-            refreshRateSeconds = 1000 * 60 * 5
-            disabled = False
-        elif "fifthteenMinuteLi" in changedId:
-            refreshRateText = "15m"
-            refreshRateSeconds = 1000 * 60 * 15
-            disabled = False
-        elif "thirtyMinuteLi" in changedId:
-            refreshRateText = "30m"
-            refreshRateSeconds = 1000 * 60 * 30
-            disabled = False
-        elif "oneHourLi" in changedId:
-            refreshRateText = "1h"
-            refreshRateSeconds = 1000 * 60 * 60
-            disabled = False
-        elif "twoHourLi" in changedId:
-            refreshRateText = "2h"
-            refreshRateSeconds = 1000 * 60 * 60 * 2
-            disabled = False
-        elif "oneDayLi" in changedId:
-            refreshRateText = "1d"
-            refreshRateSeconds = 1000 * 60 * 60 * 24
-            disabled = False
-        else:
-            refreshRateText = "Off"
-            refreshRateSeconds = 1000
-            disabled = True
-
-        return [refreshRateText + " ", html.Span(className = "caret")], refreshRateSeconds, disabled
+    def interval(*args, **kwargs):
+        return helpers.interval(*args, **kwargs)
