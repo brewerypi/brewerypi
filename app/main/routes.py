2fc530030ca45feaeb604efbed4befd943aa9af7
Brian Faivre <brian.faivre@gmail.com>
2020-08-07 09:11:05 -0700

    Removed GitPython code for tag/branch.

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,21 +1,7 @@
-from os import getcwd
 from flask import render_template
-from git import Repo
 from . import main
 
 @main.route("/")
 def index():
-	repository = Repo(getcwd())
-	tagName = ""
-	for tag in repository.tags:
-		if tag.commit == repository.head.commit:
-			tagName = tag.name
-			break
-
-	if tagName == "":
-		branch = repository.active_branch
-		version = branch.name
-	else:
-		version = tagName
-
+	version = "AWS"
 	return render_template("main/index.html", version = version)
