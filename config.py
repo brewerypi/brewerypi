281d5123b76de1f866dd8a99778556a8e481a3d9
Brian Faivre <brian.faivre@gmail.com>
2020-08-11 23:34:16 -0700

    Build SQL Alchemy URIs w/existing .env variables.

--- config.py
+++ config.py
@@ -20,8 +20,11 @@ class Config:
 	IS_RASPBERRY_PI = True if os.environ.get("IS_RASPBERRY_PI") == "1" else False
 	LOCAL_TIMEZONE = os.environ.get("LOCAL_TIMEZONE")
 	MYSQL_DATABASE = os.environ.get("MYSQL_DATABASE") or "BreweryPi"
+	MYSQL_HOST = os.environ.get("MYSQL_HOST") or "localhost"
+	MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD") or "brewery"
+	MYSQL_USERNAME = os.environ.get("MYSQL_USERNAME") or "pi"
 	NOTIFICATIONS_INTERVAL_IN_MILLISECONDS = os.environ.get("NOTIFICATIONS_INTERVAL") or 10000
 	SECRET_KEY = os.environ.get("SECRET_KEY") or "Replace with a hard to guess string."
-	SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI") or "mysql://pi:brewery@localhost/BreweryPi"
-	SQLALCHEMY_SERVER_URI = os.environ.get("SQLALCHEMY_SERVER_URI") or "mysql://pi:brewery@localhost"
+	SQLALCHEMY_DATABASE_URI = f"mysql://{MYSQL_USERNAME}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DATABASE}"
+	SQLALCHEMY_SERVER_URI = f"mysql://{MYSQL_USERNAME}:{MYSQL_PASSWORD}@{MYSQL_HOST}"
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
