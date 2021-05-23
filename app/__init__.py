b0a6ae68c44fd75d7a474592b8d3a5f64d929787
Brian Faivre <brian.faivre@gmail.com>
2021-05-23 08:44:28 -0400

    Bumped version to v3.2.2.

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
 
-__version__ = "v3.2.1"
+__version__ = "v3.2.2"
 boostrap = Bootstrap()
 db = SQLAlchemy()
 loginManager = LoginManager()
